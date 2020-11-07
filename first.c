cb9f4bc814dab4787627fce3ebbaa33f4afd6583
SofyaShestakova <soneshya@mail.ru>
2020-11-07 21:01:31 +0300

    Add semaphores, add read void and search of minimum value

--- first.c
+++ first.c
@@ -4,12 +4,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <semaphore.h>
 
-#define A 316*2^20
+#define A (316<<20)
 #define B 0x7153C850
 #define C malloc
 #define D 84
-#define E 197*2^20
+#define E (197<<20)
 #define F nocache
 #define G 112
 #define H random
@@ -17,7 +18,13 @@
 #define J min
 #define K sem
 
-void* memory_start;
+
+sem_t semaphore;
+
+void *memory_start;
+int files_amount = A / E + (A % E > 0 ? 1 : 0);
+char result_name[13];
+int file_size = E;
 
 void createPartOfMemory() {
     //до аллокации
@@ -37,34 +44,64 @@ void createPartOfMemory() {
     memory_start = start;
 }
 
-void destroyPartOfMemory(){
+void destroyPartOfMemory() {
     free(memory_start);
 }
 
 void write_files() {
     int block_size = G;
-    int files_amount = A / E + (A % E > 0 ? 1 : 0);
-    int file_size = E;
-    int file_blocks_amount = E/G + (E% G > 0 ? 1 : 0);
+    int file_blocks_amount = E / G + (E % G > 0 ? 1 : 0);
 
     int cur_file_idx = 0; // index of a current file
     for (;;) {
-        if(cur_file_idx==file_size) break;
-        char result_name[13];
-        snprintf(result_name,13, "lab_os_%d.bin", cur_file_idx);
+        if (cur_file_idx == files_amount) break;
+
+        sem_wait(&semaphore);
+        snprintf(result_name, 13, "lab_os_%d.bin", cur_file_idx);
         int flag = O_WRONLY | O_CREAT | O_TRUNC | O_DIRECT;
-        FILE * file  = fopen(result_name, flag);
-        for(int i=0;i<file_blocks_amount;i++){
-            fwrite( memory_start + (cur_file_idx*file_blocks_amount+i)*block_size, 1, block_size, file);
+        FILE *file = fopen(result_name, flag);
+        for (int i = 0; i < file_blocks_amount; i++) {
+            fwrite(memory_start + (cur_file_idx * file_blocks_amount + i) * block_size, 1, block_size, file);
         }
+        sem_post(&semaphore);
+
         cur_file_idx++;
     }
 }
+void read_and_search_min(){
+    int threads_amount = I;
+    pthread_t new_threads[threads_amount];
+    int min = INT_MAX;
 
+    void *thread_func() {
+       int* numbers =  malloc(file_size);
+        for(int i=0;i<files_amount;i++){
+            snprintf(result_name, 13, "lab_os_%d.bin", i);
+            FILE *file =  fopen(result_name, "r");
+            int read_numbers = read(file,numbers,file_size)/sizeof(int);
+            for(int j=0;j<read_numbers;++j){
+                int number = numbers[j];
+                sem_wait(&semaphore);
+                if (number < min) {
+                    min = number;
+                }
+                sem_post(&semaphore);
+            }
+        }
+    }
+    for(int i=0;i<threads_amount;i++){
+        pthread_create(&new_threads[i],NULL,*thread_func,NULL);
+    }
+    for(int i=0;i<threads_amount;i++){
+        pthread_join(&new_threads[i],NULL);
+    }
+}
 
 void main() {
-        createPartOfMemory();
-        write_files();
-        destroyPartOfMemory();
-
-    }
+    sem_init(&semaphore, 0, 1);
+    createPartOfMemory();
+    write_files();
+    read_and_search_min();
+    destroyPartOfMemory();
+    sem_destroy(&semaphore);
+}
